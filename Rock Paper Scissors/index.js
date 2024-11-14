6de1f8ebf249262295e53d23426a85ce19688bf6
Muayyad822 <muayyad822@gmail.com>
2024-11-14 11:16:19 +0100

    konga updated

---
+++ Rock Paper Scissors/index.js
@@ -0,0 +1,24 @@
+function play(yourChoice) {
+    const choices = ["rock", "paper", "scissors"];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    
+    let result = "";
+    
+    if (yourChoice == computerChoice) {
+        result = "It's a tie!";
+    }
+    else if (
+        (yourChoice == "rock" && computerChoice == "scissors") ||
+        (yourChoice == "paper" && computerChoice == "rock") ||
+        (yourChoice == "scissors" && computerChoice == "paper")
+    ){
+        result = "You win!";
+    }
+    else {
+        result = "Computer wins!";
+    }
+
+    document.getElementById("yourChoice").textContent = `You picked: ${yourChoice}`; 
+    document.getElementById("computerChoice").textContent =  `Computer picked: ${computerChoice}`; 
+    document.getElementById("result").textContent =  result;
+}
