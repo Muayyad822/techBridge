92f87534b4616628b50839579ead79673d10af5f
Muayyad822 <muayyad822@gmail.com>
2024-11-28 21:25:54 +0100

    Konga updated

--- Konga/index.js
+++ Konga/index.js
@@ -1,1 +1,13 @@
-cartCount = document.getElementById("cartCount").textContent;
+let cartCount = document.querySelector("#cartCount");
+let count = 0;
+let addButtons = document.querySelectorAll(".add")
+
+for (let i = 0; i < addButtons.length; i++) {
+   addButtons[i].addEventListener("click", function(){
+    count++;
+    cartCount.innerHTML = count;
+  
+})
+    
+}
+
